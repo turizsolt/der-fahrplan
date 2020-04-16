5bcef52374531f2181af72754cb9ff542bb8a34e
turizsolt <turi.zsolt.m@gmail.com>
2020-04-16 20:29:21 +0200

    feat: first two levels

--- src/levels/FirstLevel.ts
+++ src/levels/FirstLevel.ts
@@ -1,345 +1,126 @@
 export const FIRST_LEVEL = {
   data: [
     {
-      id: 'gikKmtcoH',
+      id: 'nlqnQxayP',
       type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: -110, dirXZ: 0 },
-      A: { track: 'HCE6iUuz*Q', whichEnd: 'B' },
-      B: { track: '45_UbwVfc', whichEnd: 'A' }
-    },
-    {
-      id: '9Zdv4HzO1q',
-      type: 'TrackJoint',
-      ray: { x: -20, y: 0, z: -140, dirXZ: -1.5707963267948966 },
-      A: { track: '5Iphq39Ae4', whichEnd: 'B' },
-      B: { track: 'HCE6iUuz*Q', whichEnd: 'A' }
-    },
-    {
-      id: 'HCE6iUuz*Q',
-      type: 'Track',
-      segment: [
-        { x: -20, y: 0, z: -140 },
-        { x: -50, y: 0, z: -140 },
-        { x: -50, y: 0, z: -110 }
-      ]
-    },
-    {
-      id: 'chrtUTd1P',
-      type: 'TrackJoint',
-      ray: { x: 10, y: 0, z: -110, dirXZ: -3.141592653589793 },
-      A: { track: 'avlJyN8i15', whichEnd: 'B' },
-      B: { track: '5Iphq39Ae4', whichEnd: 'A' }
-    },
-    {
-      id: '5Iphq39Ae4',
-      type: 'Track',
-      segment: [
-        { x: 10, y: 0, z: -110 },
-        { x: 9.999999999999996, y: 0, z: -140 },
-        { x: -20, y: 0, z: -140 }
-      ]
-    },
-    {
-      id: '3OeD92BrI',
-      type: 'TrackJoint',
-      ray: { x: -20, y: 0, z: -80, dirXZ: 1.5707963267948966 },
-      A: { track: 'jYjMTNbPhw', whichEnd: 'B' },
-      B: { track: 'avlJyN8i15', whichEnd: 'A' }
-    },
-    {
-      id: 'avlJyN8i15',
-      type: 'Track',
-      segment: [
-        { x: -20, y: 0, z: -80 },
-        { x: 10.000000000000004, y: 0, z: -80 },
-        { x: 10, y: 0, z: -110 }
-      ]
-    },
-    {
-      id: '9qy3LYqIW',
-      type: 'TrackJoint',
-      ray: { x: -40, y: 0, z: -70, dirXZ: 2.356194490192345 },
-      A: { track: '3HL4IEnDaj', whichEnd: 'F' },
-      B: { track: 'jYjMTNbPhw', whichEnd: 'A' }
-    },
-    {
-      id: 'jYjMTNbPhw',
-      type: 'Track',
-      segment: [
-        { x: -40, y: 0, z: -70 },
-        { x: -30, y: 0, z: -80 },
-        { x: -20, y: 0, z: -80 }
-      ]
-    },
-    {
-      id: 'ZOSxx8j6D',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: -50, dirXZ: 3.141592653589793 },
-      A: { track: 'JGf08XBHUr', whichEnd: 'B' },
-      B: { track: '3HL4IEnDaj', whichEnd: 'A' }
-    },
-    {
-      id: 'QFxNCYymD',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: -70, dirXZ: 3.141592653589793 },
-      A: { track: '3HL4IEnDaj', whichEnd: 'E' },
-      B: { track: '45_UbwVfc', whichEnd: 'B' }
-    },
-    {
-      id: '3HL4IEnDaj',
-      type: 'TrackSwitch',
-      segmentE: [
-        { x: -50, y: 0, z: -50 },
-        { x: -50, y: 0, z: -60.00000000000001 },
-        { x: -40, y: 0, z: -70 }
-      ],
-      segmentF: [{ x: -50, y: 0, z: -50 }, { x: -50, y: 0, z: -70 }]
-    },
-    {
-      id: '45_UbwVfc',
-      type: 'Track',
-      segment: [{ x: -50, y: 0, z: -110 }, { x: -50, y: 0, z: -70 }]
-    },
-    {
-      id: 'iwL1j6Ecz',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: 0, dirXZ: 3.141592653589793 },
-      A: { track: 'RVUhPsGgOt', whichEnd: 'A' },
-      B: { track: 'JGf08XBHUr', whichEnd: 'A' }
-    },
-    {
-      id: 'JGf08XBHUr',
-      type: 'Track',
-      segment: [{ x: -50, y: 0, z: 0 }, { x: -50, y: 0, z: -50 }]
-    },
-    {
-      id: 'ssBzC14pM',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: 20, dirXZ: -3.141592653589793 },
-      A: { track: '79YQrzKIHP', whichEnd: 'B' },
-      B: { track: 'RVUhPsGgOt', whichEnd: 'F' }
-    },
-    {
-      id: 'JwgMGGwzV',
-      type: 'TrackJoint',
-      ray: { x: -45, y: 0, z: 20, dirXZ: -2.356194490192345 },
-      A: { track: 'hnLdZKeoQc', whichEnd: 'B' },
-      B: { track: 'RVUhPsGgOt', whichEnd: 'E' }
-    },
-    {
-      id: 'RVUhPsGgOt',
-      type: 'TrackSwitch',
-      segmentE: [{ x: -50, y: 0, z: 0 }, { x: -50, y: 0, z: 20 }],
-      segmentF: [
-        { x: -50, y: 0, z: 0 },
-        { x: -50, y: 0, z: 14.999999999999993 },
-        { x: -45, y: 0, z: 20 }
-      ]
-    },
-    {
-      id: 'kGJ2_r*yn',
-      type: 'TrackJoint',
-      ray: { x: -40, y: 0, z: 40, dirXZ: -3.141592653589793 },
-      A: { track: '2qS6zRqsk', whichEnd: 'A' },
-      B: { track: 'hnLdZKeoQc', whichEnd: 'A' }
-    },
-    {
-      id: 'hnLdZKeoQc',
-      type: 'Track',
-      segment: [
-        { x: -40, y: 0, z: 40 },
-        { x: -40.00000000000001, y: 0, z: 24.999999999999993 },
-        { x: -45, y: 0, z: 20 }
-      ]
-    },
-    {
-      id: 'gAkNHyGRL',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: 60, dirXZ: -3.141592653589793 },
-      A: { track: 'ppkyFe8S4k', whichEnd: 'F' },
-      B: { track: '79YQrzKIHP', whichEnd: 'A' }
-    },
-    {
-      id: '79YQrzKIHP',
-      type: 'Track',
-      segment: [{ x: -50, y: 0, z: 60 }, { x: -50, y: 0, z: 20 }]
-    },
-    {
-      id: 'cMi9aPVe7',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: 80, dirXZ: -3.141592653589793 },
-      A: { track: 'cms03eVJcU', whichEnd: 'B' },
-      B: { track: 'ppkyFe8S4k', whichEnd: 'A' }
-    },
-    {
-      id: 'j7J4A8hKQ',
-      type: 'TrackJoint',
-      ray: { x: -45, y: 0, z: 60, dirXZ: 2.356194490192345 },
-      A: { track: 'ppkyFe8S4k', whichEnd: 'E' },
-      B: { track: '2qS6zRqsk', whichEnd: 'B' }
-    },
-    {
-      id: 'ppkyFe8S4k',
-      type: 'TrackSwitch',
-      segmentE: [{ x: -50, y: 0, z: 80 }, { x: -50, y: 0, z: 60 }],
-      segmentF: [
-        { x: -50, y: 0, z: 80 },
-        { x: -50, y: 0, z: 65 },
-        { x: -45, y: 0, z: 60 }
-      ]
-    },
-    {
-      id: '2qS6zRqsk',
-      type: 'Track',
-      segment: [
-        { x: -40, y: 0, z: 40 },
-        { x: -40, y: 0, z: 55 },
-        { x: -45, y: 0, z: 60 }
-      ]
-    },
-    {
-      id: 'RwbbE00j7',
-      type: 'TrackJoint',
-      ray: { x: -50, y: 0, z: 100, dirXZ: 3.141592653589793 },
+      ray: { x: -10, y: 0, z: 5, dirXZ: 0 },
       A: null,
-      B: { track: 'cms03eVJcU', whichEnd: 'A' }
+      B: { track: 'I*_HisyxV_', whichEnd: 'B' }
     },
     {
-      id: 'cms03eVJcU',
+      id: 'x_V6aidT53',
+      type: 'TrackJoint',
+      ray: { x: -10, y: 0, z: 75, dirXZ: 0 },
+      A: { track: 'I*_HisyxV_', whichEnd: 'A' },
+      B: null
+    },
+    {
+      id: 'I*_HisyxV_',
       type: 'Track',
-      segment: [{ x: -50, y: 0, z: 100 }, { x: -50, y: 0, z: 80 }]
+      segment: [{ x: -10, y: 0, z: 75 }, { x: -10, y: 0, z: 5 }]
     },
     {
-      id: 'KPohK7GeD',
+      id: 'W23_0EtKL',
+      circle: { x: -15, y: 0, z: 20, r: 15 },
+      type: 'Station',
+      name: 'Aelcoast'
+    },
+    {
+      id: '*ap0xjNun',
+      circle: { x: -15, y: 0, z: 60, r: 15 },
+      type: 'Station',
+      name: 'Oakbarrow'
+    },
+    {
+      id: 'Mglj_usJe',
       type: 'Platform',
-      track: '79YQrzKIHP',
-      start: 0.125,
+      track: 'I*_HisyxV_',
+      start: 0.625,
       end: 0.875,
       side: 'Right',
       width: 7.5
     },
     {
-      id: '1wn*9NB9J',
-      type: 'Wagon',
-      seatCount: 21,
-      seatColumns: 3,
-      seats: [],
-      A: {
-        whichEnd: 'A',
-        endOf: '1wn*9NB9J',
-        positionOnTrack: {
-          track: '79YQrzKIHP',
-          position: 21,
-          percentage: 0.525,
-          direction: 1
-        },
-        otherEnd: { endOf: 'C*t4dyORV', whichEnd: 'B' }
-      },
-      B: {
-        whichEnd: 'B',
-        endOf: '1wn*9NB9J',
-        positionOnTrack: {
-          track: '79YQrzKIHP',
-          position: 35,
-          percentage: 0.875,
-          direction: 1
-        },
-        otherEnd: false
-      },
-      train: '_OZ91Zwy0x'
-    },
-    {
-      id: 'C*t4dyORV',
-      type: 'Wagon',
-      seatCount: 21,
-      seatColumns: 3,
-      seats: [],
-      A: {
-        whichEnd: 'A',
-        endOf: 'C*t4dyORV',
-        positionOnTrack: {
-          track: '79YQrzKIHP',
-          position: 6,
-          percentage: 0.15,
-          direction: 1
-        },
-        otherEnd: false
-      },
-      B: {
-        whichEnd: 'B',
-        endOf: 'C*t4dyORV',
-        positionOnTrack: {
-          track: '79YQrzKIHP',
-          position: 20,
-          percentage: 0.5,
-          direction: 1
-        },
-        otherEnd: { endOf: '1wn*9NB9J', whichEnd: 'A' }
-      },
-      train: '_OZ91Zwy0x'
-    },
-    {
-      id: '_OZ91Zwy0x',
-      type: 'Train',
-      wagons: ['C*t4dyORV', '1wn*9NB9J'],
-      schedulingWagon: 'C*t4dyORV'
-    },
-    {
-      id: 'NIGVDSeTt',
+      id: 'XRByNvr43',
       type: 'Platform',
-      track: '45_UbwVfc',
+      track: 'I*_HisyxV_',
       start: 0.125,
-      end: 0.875,
-      side: 'Left',
+      end: 0.375,
+      side: 'Right',
       width: 7.5
     },
     {
-      id: 'p3RcHS_Mr',
-      circle: { x: -55, y: 0, z: -90, r: 10 },
-      type: 'Station',
-      name: 'Pondley'
+      id: 'A2nPdwsy8',
+      type: 'Wagon',
+      seatCount: 21,
+      seatColumns: 3,
+      seats: [],
+      A: {
+        whichEnd: 'A',
+        endOf: 'A2nPdwsy8',
+        positionOnTrack: {
+          track: 'I*_HisyxV_',
+          position: 45,
+          percentage: 0.6428571428571429,
+          direction: 1
+        },
+        otherEnd: false
+      },
+      B: {
+        whichEnd: 'B',
+        endOf: 'A2nPdwsy8',
+        positionOnTrack: {
+          track: 'I*_HisyxV_',
+          position: 59,
+          percentage: 0.8428571428571429,
+          direction: 1
+        },
+        otherEnd: false
+      },
+      trip: null,
+      train: 'RRaeHFfiMH'
     },
     {
-      id: 'ut3e1ecqG',
-      circle: { x: -55, y: 0, z: 40, r: 10 },
-      type: 'Station',
-      name: 'Deepsage'
+      id: 'RRaeHFfiMH',
+      type: 'Train',
+      wagons: ['A2nPdwsy8'],
+      schedulingWagon: 'A2nPdwsy8'
     },
     {
-      id: 'g5bIHrj*q',
+      id: 'mFEeGSi7D',
       type: 'Route',
       name: '1',
-      stops: ['RfYJAm7Dj', 'pZkBXBbJR']
+      stops: ['lIF4xyOGE', 'jcXLbpcdJ']
     },
     {
-      id: 'RfYJAm7Dj',
+      id: 'lIF4xyOGE',
       type: 'RouteStop',
-      station: 'p3RcHS_Mr',
-      platform: 'NIGVDSeTt'
+      station: 'W23_0EtKL',
+      platform: 'Mglj_usJe'
     },
     {
-      id: 'pZkBXBbJR',
+      id: 'jcXLbpcdJ',
       type: 'RouteStop',
-      station: 'ut3e1ecqG',
-      platform: 'KPohK7GeD'
+      station: '*ap0xjNun',
+      platform: 'XRByNvr43'
     },
     {
-      id: 'ELrfnjoum',
+      id: 'kTZFipKmw',
       type: 'Route',
       name: '1',
-      stops: ['gy4xak6xS*', 'r1MkmQQrgw']
+      stops: ['KmIltK66Cv', '9O3lK9M*gN']
     },
     {
-      id: 'gy4xak6xS*',
+      id: 'KmIltK66Cv',
       type: 'RouteStop',
-      station: 'ut3e1ecqG',
-      platform: 'KPohK7GeD'
+      station: '*ap0xjNun',
+      platform: 'XRByNvr43'
     },
     {
-      id: 'r1MkmQQrgw',
+      id: '9O3lK9M*gN',
       type: 'RouteStop',
-      station: 'p3RcHS_Mr',
-      platform: 'NIGVDSeTt'
+      station: 'W23_0EtKL',
+      platform: 'Mglj_usJe'
     }
   ],
   _version: 1,
--- src/levels/SecondLevel.ts
+++ src/levels/SecondLevel.ts
@@ -1,367 +1,286 @@
 export const SECOND_LEVEL = {
   data: [
     {
-      id: 'OUhbzoBdt',
+      id: 'glO38U0kq',
       type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: 130, dirXZ: 0 },
-      A: { track: 'peBmZ84LLH', whichEnd: 'B' },
+      ray: { x: -10, y: 0, z: 50, dirXZ: 0.7853981633974483 },
+      A: { track: '_ICP1oo2Y0', whichEnd: 'B' },
+      B: { track: 'FKgIsWamx', whichEnd: 'E' }
+    },
+    {
+      id: 'RN9oz6f*wa',
+      type: 'TrackJoint',
+      ray: { x: -10, y: 0, z: 30, dirXZ: -0.7853981633974483 },
+      A: { track: 'Q1eGdkBRd', whichEnd: 'F' },
+      B: { track: '_ICP1oo2Y0', whichEnd: 'A' }
+    },
+    {
+      id: '_ICP1oo2Y0',
+      type: 'Track',
+      segment: [
+        { x: -10, y: 0, z: 30 },
+        { x: -19.999999999999996, y: 0, z: 40 },
+        { x: -10, y: 0, z: 50 }
+      ]
+    },
+    {
+      id: 'UzmKvr9NL',
+      type: 'TrackJoint',
+      ray: { x: 0, y: 0, z: 30, dirXZ: 0.7853981633974483 },
+      A: { track: 'Q1eGdkBRd', whichEnd: 'E' },
+      B: { track: 'Bk1c5_55_2', whichEnd: 'B' }
+    },
+    {
+      id: 'w6DlNTLR9l',
+      type: 'TrackJoint',
+      ray: { x: 0, y: 0, z: 50, dirXZ: -0.7853981633974483 },
+      A: { track: 'Bk1c5_55_2', whichEnd: 'A' },
+      B: { track: 'FKgIsWamx', whichEnd: 'F' }
+    },
+    {
+      id: 'Bk1c5_55_2',
+      type: 'Track',
+      segment: [
+        { x: 0, y: 0, z: 50 },
+        { x: 10, y: 0, z: 40 },
+        { x: 0, y: 0, z: 30 }
+      ]
+    },
+    {
+      id: 'Kx6LdYqfS',
+      type: 'TrackJoint',
+      ray: { x: -5, y: 0, z: 65, dirXZ: -3.141592653589793 },
+      A: { track: 'KIWDpK_*4k', whichEnd: 'B' },
+      B: { track: 'FKgIsWamx', whichEnd: 'A' }
+    },
+    {
+      id: 'FKgIsWamx',
+      type: 'TrackSwitch',
+      segmentE: [
+        { x: -5, y: 0, z: 65 },
+        { x: -5.000000000000001, y: 0, z: 55 },
+        { x: 0, y: 0, z: 50 }
+      ],
+      segmentF: [
+        { x: -5, y: 0, z: 65 },
+        { x: -5, y: 0, z: 55 },
+        { x: -10, y: 0, z: 50 }
+      ]
+    },
+    {
+      id: '7Rdz6xYit',
+      type: 'TrackJoint',
+      ray: { x: -5, y: 0, z: 15, dirXZ: -3.141592653589793 },
+      A: { track: 'Q1eGdkBRd', whichEnd: 'A' },
+      B: { track: 'SEvaD8qJyH', whichEnd: 'B' }
+    },
+    {
+      id: 'Q1eGdkBRd',
+      type: 'TrackSwitch',
+      segmentE: [
+        { x: -5, y: 0, z: 15 },
+        { x: -4.999999999999999, y: 0, z: 25 },
+        { x: -10, y: 0, z: 30 }
+      ],
+      segmentF: [
+        { x: -5, y: 0, z: 15 },
+        { x: -4.999999999999999, y: 0, z: 25 },
+        { x: 0, y: 0, z: 30 }
+      ]
+    },
+    {
+      id: '9E24Ca*CO',
+      type: 'TrackJoint',
+      ray: { x: -20, y: 0, z: 0, dirXZ: -1.5707963267948966 },
+      A: { track: 'SEvaD8qJyH', whichEnd: 'A' },
+      B: { track: 'mbXMdJiOR1', whichEnd: 'B' }
+    },
+    {
+      id: 'SEvaD8qJyH',
+      type: 'Track',
+      segment: [
+        { x: -20, y: 0, z: 0 },
+        { x: -5.000000000000002, y: 0, z: 0 },
+        { x: -5, y: 0, z: 15 }
+      ]
+    },
+    {
+      id: 'TFgsdR6h9',
+      type: 'TrackJoint',
+      ray: { x: -60, y: 0, z: 0, dirXZ: -1.5707963267948966 },
+      A: { track: 'mbXMdJiOR1', whichEnd: 'A' },
       B: null
     },
     {
-      id: '3G2EolxT7n',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: 90, dirXZ: 0 },
-      A: { track: '8f4CSUYdWx', whichEnd: 'B' },
-      B: { track: 'peBmZ84LLH', whichEnd: 'A' }
-    },
-    {
-      id: 'peBmZ84LLH',
+      id: 'mbXMdJiOR1',
       type: 'Track',
-      segment: [{ x: -30, y: 0, z: 90 }, { x: -30, y: 0, z: 130 }]
+      segment: [{ x: -60, y: 0, z: 0 }, { x: -20, y: 0, z: 0 }]
     },
     {
-      id: 'jEnbFJtc8',
+      id: 'W9N0wk43Q',
       type: 'TrackJoint',
-      ray: { x: -20, y: 0, z: 90, dirXZ: 0 },
-      A: { track: 'm2hoqdIFW', whichEnd: 'B' },
-      B: { track: 'OtmrIDvsWZ', whichEnd: 'B' }
+      ray: { x: -20, y: 0, z: 80, dirXZ: -1.5707963267948966 },
+      A: { track: 'KIWDpK_*4k', whichEnd: 'A' },
+      B: { track: 'Nn1senKJdY', whichEnd: 'B' }
     },
     {
-      id: '2PDjJL0LZK',
+      id: 'KIWDpK_*4k',
+      type: 'Track',
+      segment: [
+        { x: -20, y: 0, z: 80 },
+        { x: -4.999999999999998, y: 0, z: 80 },
+        { x: -5, y: 0, z: 65 }
+      ]
+    },
+    {
+      id: 'L4R1sJGIj',
       type: 'TrackJoint',
-      ray: { x: -20, y: 0, z: 130, dirXZ: 0 },
-      A: { track: 'OtmrIDvsWZ', whichEnd: 'A' },
+      ray: { x: -60, y: 0, z: 80, dirXZ: -1.5707963267948966 },
+      A: { track: 'Nn1senKJdY', whichEnd: 'A' },
       B: null
     },
     {
-      id: 'OtmrIDvsWZ',
+      id: 'Nn1senKJdY',
       type: 'Track',
-      segment: [{ x: -20, y: 0, z: 130 }, { x: -20, y: 0, z: 90 }]
+      segment: [{ x: -60, y: 0, z: 80 }, { x: -20, y: 0, z: 80 }]
     },
     {
-      id: '18M9zLxAB',
-      type: 'TrackJoint',
-      ray: { x: -25, y: 0, z: 70, dirXZ: 0.7853981633974483 },
-      A: { track: '155T_TAGG', whichEnd: 'E' },
-      B: { track: 'm2hoqdIFW', whichEnd: 'A' }
-    },
-    {
-      id: 'm2hoqdIFW',
-      type: 'Track',
-      segment: [
-        { x: -25, y: 0, z: 70 },
-        { x: -20, y: 0, z: 75 },
-        { x: -20, y: 0, z: 90 }
-      ]
-    },
-    {
-      id: 'mMfkOCBqU',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: 70, dirXZ: 0 },
-      A: { track: '155T_TAGG', whichEnd: 'F' },
-      B: { track: '8f4CSUYdWx', whichEnd: 'A' }
-    },
-    {
-      id: '8f4CSUYdWx',
-      type: 'Track',
-      segment: [{ x: -30, y: 0, z: 70 }, { x: -30, y: 0, z: 90 }]
-    },
-    {
-      id: '3I3MdrW63',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: 50, dirXZ: 0 },
-      A: { track: '1Ze0WNuCZq', whichEnd: 'B' },
-      B: { track: '155T_TAGG', whichEnd: 'A' }
-    },
-    {
-      id: '155T_TAGG',
-      type: 'TrackSwitch',
-      segmentE: [{ x: -30, y: 0, z: 50 }, { x: -30, y: 0, z: 70 }],
-      segmentF: [
-        { x: -30, y: 0, z: 50 },
-        { x: -30, y: 0, z: 65 },
-        { x: -25, y: 0, z: 70 }
-      ]
-    },
-    {
-      id: 'pE1pru*kI',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: 10, dirXZ: 0 },
-      A: { track: 'p5bW2kobiN', whichEnd: 'A' },
-      B: { track: '1Ze0WNuCZq', whichEnd: 'A' }
-    },
-    {
-      id: '1Ze0WNuCZq',
-      type: 'Track',
-      segment: [{ x: -30, y: 0, z: 10 }, { x: -30, y: 0, z: 50 }]
-    },
-    {
-      id: 'IZJwITYqc',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: -10, dirXZ: 0 },
-      A: { track: '6rxglrnQS*', whichEnd: 'A' },
-      B: { track: 'p5bW2kobiN', whichEnd: 'F' }
-    },
-    {
-      id: 'moDSsdBym',
-      type: 'TrackJoint',
-      ray: { x: -25, y: 0, z: -10, dirXZ: -0.7853981633974483 },
-      A: { track: 'UacAkVZCx7', whichEnd: 'B' },
-      B: { track: 'p5bW2kobiN', whichEnd: 'E' }
-    },
-    {
-      id: 'p5bW2kobiN',
-      type: 'TrackSwitch',
-      segmentE: [{ x: -30, y: 0, z: 10 }, { x: -30, y: 0, z: -10 }],
-      segmentF: [
-        { x: -30, y: 0, z: 10 },
-        { x: -30, y: 0, z: -5.000000000000001 },
-        { x: -25, y: 0, z: -10 }
-      ]
-    },
-    {
-      id: 'VdQ0hVMYk',
-      type: 'TrackJoint',
-      ray: { x: -20, y: 0, z: -30, dirXZ: 0 },
-      A: { track: 'RqRx531yr0', whichEnd: 'A' },
-      B: { track: 'UacAkVZCx7', whichEnd: 'A' }
-    },
-    {
-      id: 'UacAkVZCx7',
-      type: 'Track',
-      segment: [
-        { x: -20, y: 0, z: -30 },
-        { x: -20, y: 0, z: -15.000000000000002 },
-        { x: -25, y: 0, z: -10 }
-      ]
-    },
-    {
-      id: '38ub1LaUp',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: -30, dirXZ: 0 },
-      A: { track: 'ZkUmXslWoW', whichEnd: 'B' },
-      B: { track: '6rxglrnQS*', whichEnd: 'B' }
-    },
-    {
-      id: '6rxglrnQS*',
-      type: 'Track',
-      segment: [{ x: -30, y: 0, z: -10 }, { x: -30, y: 0, z: -30 }]
-    },
-    {
-      id: 'XNC_qKvIF',
-      type: 'TrackJoint',
-      ray: { x: -30, y: 0, z: -70, dirXZ: 0 },
-      A: null,
-      B: { track: 'ZkUmXslWoW', whichEnd: 'A' }
-    },
-    {
-      id: 'ZkUmXslWoW',
-      type: 'Track',
-      segment: [{ x: -30, y: 0, z: -70 }, { x: -30, y: 0, z: -30 }]
-    },
-    {
-      id: '7*X_IrsNU',
-      type: 'TrackJoint',
-      ray: { x: -20, y: 0, z: -70, dirXZ: 0 },
-      A: null,
-      B: { track: 'RqRx531yr0', whichEnd: 'B' }
-    },
-    {
-      id: 'RqRx531yr0',
-      type: 'Track',
-      segment: [{ x: -20, y: 0, z: -30 }, { x: -20, y: 0, z: -70 }]
-    },
-    {
-      id: 'rdbfllwFe',
+      id: 'ep4WE3VbL',
       type: 'Platform',
-      track: 'ZkUmXslWoW',
-      start: 0.125,
-      end: 0.875,
+      track: 'mbXMdJiOR1',
+      start: 0,
+      end: 0.35,
       side: 'Left',
       width: 7.5
     },
     {
-      id: 'mvTBXriFH',
+      id: 's*3B6Z6cD',
       type: 'Platform',
-      track: 'RqRx531yr0',
-      start: 0.125,
-      end: 0.875,
-      side: 'Left',
+      track: 'Nn1senKJdY',
+      start: 0,
+      end: 0.35,
+      side: 'Right',
       width: 7.5
     },
     {
-      id: '*tsQQZKqd',
-      type: 'Platform',
-      track: 'peBmZ84LLH',
-      start: 0.125,
-      end: 0.875,
-      side: 'Left',
-      width: 7.5
-    },
-    {
-      id: 'wnNUUCsUX',
-      type: 'Platform',
-      track: 'OtmrIDvsWZ',
-      start: 0.125,
-      end: 0.875,
-      side: 'Left',
-      width: 7.5
-    },
-    {
-      id: 'ntia497tF',
+      id: 'mJ*jZ_hyP',
       type: 'Wagon',
       seatCount: 21,
       seatColumns: 3,
       seats: [],
       A: {
         whichEnd: 'A',
-        endOf: 'ntia497tF',
+        endOf: 'mJ*jZ_hyP',
         positionOnTrack: {
-          track: 'ZkUmXslWoW',
+          track: 'mbXMdJiOR1',
+          position: 0,
+          percentage: 0,
+          direction: 1
+        },
+        otherEnd: false
+      },
+      B: {
+        whichEnd: 'B',
+        endOf: 'mJ*jZ_hyP',
+        positionOnTrack: {
+          track: 'mbXMdJiOR1',
           position: 14,
           percentage: 0.35,
           direction: 1
         },
         otherEnd: false
       },
-      B: {
-        whichEnd: 'B',
-        endOf: 'ntia497tF',
-        positionOnTrack: {
-          track: 'ZkUmXslWoW',
-          position: 28,
-          percentage: 0.7,
-          direction: 1
-        },
-        otherEnd: false
-      },
-      train: 'E3mdKzu1tG'
+      train: 'DNQcIiN7Ky'
     },
     {
-      id: 'E3mdKzu1tG',
+      id: 'DNQcIiN7Ky',
       type: 'Train',
-      wagons: ['ntia497tF'],
-      schedulingWagon: 'ntia497tF'
+      wagons: ['mJ*jZ_hyP'],
+      schedulingWagon: 'mJ*jZ_hyP'
     },
     {
-      id: 'ZhwF9QnFs',
+      id: 'GCzfSYiQg',
       type: 'Wagon',
       seatCount: 21,
       seatColumns: 3,
       seats: [],
       A: {
         whichEnd: 'A',
-        endOf: 'ZhwF9QnFs',
+        endOf: 'GCzfSYiQg',
         positionOnTrack: {
-          track: 'peBmZ84LLH',
+          track: 'Nn1senKJdY',
+          position: 0,
+          percentage: 0,
+          direction: 1
+        },
+        otherEnd: false
+      },
+      B: {
+        whichEnd: 'B',
+        endOf: 'GCzfSYiQg',
+        positionOnTrack: {
+          track: 'Nn1senKJdY',
           position: 14,
           percentage: 0.35,
           direction: 1
         },
         otherEnd: false
       },
-      B: {
-        whichEnd: 'B',
-        endOf: 'ZhwF9QnFs',
-        positionOnTrack: {
-          track: 'peBmZ84LLH',
-          position: 28,
-          percentage: 0.7,
-          direction: 1
-        },
-        otherEnd: false
-      },
-      train: 'bl5HKWZjfn'
+      train: 'NQkkdVwbBx'
     },
     {
-      id: 'bl5HKWZjfn',
+      id: 'NQkkdVwbBx',
       type: 'Train',
-      wagons: ['ZhwF9QnFs'],
-      schedulingWagon: 'ZhwF9QnFs'
+      wagons: ['GCzfSYiQg'],
+      schedulingWagon: 'GCzfSYiQg'
     },
     {
-      id: '2bxOIazPq',
-      type: 'Wagon',
-      seatCount: 21,
-      seatColumns: 3,
-      seats: [],
-      A: {
-        whichEnd: 'A',
-        endOf: '2bxOIazPq',
-        positionOnTrack: {
-          track: 'OtmrIDvsWZ',
-          position: 29.5108595905551,
-          percentage: 0.7377714897638775,
-          direction: -1
-        },
-        otherEnd: false
-      },
-      B: {
-        whichEnd: 'B',
-        endOf: '2bxOIazPq',
-        positionOnTrack: {
-          track: 'OtmrIDvsWZ',
-          position: 15.5108595905551,
-          percentage: 0.3877714897638775,
-          direction: -1
-        },
-        otherEnd: false
-      },
-      train: 'Z0wlPHYK1M'
-    },
-    {
-      id: 'Z0wlPHYK1M',
-      type: 'Train',
-      wagons: ['2bxOIazPq'],
-      schedulingWagon: '2bxOIazPq'
-    },
-    {
-      id: 'bP_85FUW_',
-      circle: { x: -25, y: 0, z: -50, r: 25 },
+      id: '7cioBy7xA',
+      circle: { x: -45, y: 0, z: 2.5, r: 12.5 },
       type: 'Station',
-      name: 'Fairdeer'
+      name: 'Oakbarrow'
     },
     {
-      id: '687NrIOjs',
-      circle: { x: -25, y: 0, z: 110, r: 25 },
+      id: 'cjtOMp7PM',
+      circle: { x: -45, y: 0, z: 77.5, r: 12.5 },
       type: 'Station',
-      name: 'Edgemeadow'
+      name: 'Deepsage'
     },
     {
-      id: 'kxqbLlwCL',
+      id: 'hPQB0MCYt4',
       type: 'Route',
-      name: '1',
-      stops: ['PraWWC*VW', 'Q1s6RqMjG']
+      name: '2',
+      stops: ['Czc2exolfQ', 'EjkPdS1T8']
     },
     {
-      id: 'PraWWC*VW',
+      id: 'Czc2exolfQ',
       type: 'RouteStop',
-      station: 'bP_85FUW_',
-      platform: 'rdbfllwFe'
+      station: '7cioBy7xA',
+      platform: 'ep4WE3VbL'
     },
     {
-      id: 'Q1s6RqMjG',
+      id: 'EjkPdS1T8',
       type: 'RouteStop',
-      station: '687NrIOjs',
-      platform: '*tsQQZKqd'
+      station: 'cjtOMp7PM',
+      platform: 's*3B6Z6cD'
     },
     {
-      id: '5FGTva6l*',
+      id: '*alyqWBQ1',
       type: 'Route',
-      name: '1',
-      stops: ['mHOEWYEJJv', 'jswkA3f3lx']
+      name: '2',
+      stops: ['Vj6WDtG9Fr', '5Rv7JQSER9']
     },
     {
-      id: 'mHOEWYEJJv',
+      id: 'Vj6WDtG9Fr',
       type: 'RouteStop',
-      station: '687NrIOjs',
-      platform: '*tsQQZKqd'
+      station: 'cjtOMp7PM',
+      platform: 's*3B6Z6cD'
     },
     {
-      id: 'jswkA3f3lx',
+      id: '5Rv7JQSER9',
       type: 'RouteStop',
-      station: 'bP_85FUW_',
-      platform: 'rdbfllwFe'
+      station: '7cioBy7xA',
+      platform: 'ep4WE3VbL'
     }
   ],
   _version: 1,
