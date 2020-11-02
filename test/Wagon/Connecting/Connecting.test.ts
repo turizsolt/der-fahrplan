154b13ab2a439e2fbe48dbb8dbd4bbfdf8767aa6
turizsolt <turi.zsolt.m@gmail.com>
2020-11-02 18:18:00 +0100

    refact: move utils from Controlling to a new file

--- test/Wagon/Connecting/Connecting.test.ts
+++ test/Wagon/Connecting/Connecting.test.ts
@@ -1,18 +1,12 @@
-import chai, { expect } from 'chai';
-import chaiAlmost from 'chai-almost';
-import { Store } from '../../../src/structs/Interfaces/Store';
-import { testContainer } from '../../../src/di/test.config';
-import { TYPES } from '../../../src/di/TYPES';
 import { Wagon } from '../../../src/structs/Interfaces/Wagon';
-import { getPredefinedWagonConfig } from '../../../src/structs/Actuals/Wagon/ActualWagonConfigs';
-import { WhichEnd } from '../../../src/structs/Interfaces/WhichEnd';
-chai.use(chaiAlmost());
-
-const store: Store = testContainer
-  .get<() => Store>(TYPES.FactoryOfStore)()
-  .init();
-store.clear();
-const WagonFactory: () => Wagon = () => store.create<Wagon>(TYPES.Wagon);
+import {
+  buildTrain,
+  W,
+  expectTrainSide,
+  A,
+  B,
+  expectTrainSides
+} from './utils';
 
 describe('WagonConnect and Control', () => {
   describe('single wagon', () => {
@@ -170,56 +164,3 @@ describe('WagonConnect and Control', () => {
     });
   });
 });
-
-const A = WhichEnd.A;
-const B = WhichEnd.B;
-
-enum W {
-  Loco = 'locomotive',
-  Pass = 'passenger',
-  Cont = 'controlCar',
-  Comb = 'combined'
-}
-
-function buildTrain(...wagonStrings: string[]): Wagon[] {
-  const returnedWagons: Wagon[] = [];
-  for (let wagonString of wagonStrings) {
-    returnedWagons.push(
-      WagonFactory().init(getPredefinedWagonConfig(wagonString))
-    );
-  }
-  for (let i = 1; i < returnedWagons.length; i++) {
-    returnedWagons[i - 1].getB().connect(returnedWagons[i].getA());
-  }
-
-  return returnedWagons;
-}
-
-function expectTrainSides(
-  wagons: Wagon[],
-  sides: WhichEnd[],
-  selected?: Wagon
-) {
-  for (let i = 0; i < wagons.length; i++) {
-    expect(wagons[i].getSelectedSide()).equals(
-      sides[i],
-      'offset ' + i.toString()
-    );
-  }
-  expectTrainSelected(selected);
-}
-
-function expectTrainSide(wagon: Wagon, side: WhichEnd, selected?: Wagon) {
-  expect(wagon.getSelectedSide()).equals(side);
-  expectTrainSelected(selected);
-}
-
-function expectTrainSelected(selected?: Wagon) {
-  if (selected !== undefined) {
-    if (selected) {
-      expect(store.getSelected().getId()).equals(selected.getId());
-    } else {
-      expect(store.getSelected()).equals(null);
-    }
-  }
-}
---
+++ test/Wagon/Connecting/utils.ts
@@ -0,0 +1,72 @@
+import chai, { expect } from 'chai';
+import chaiAlmost from 'chai-almost';
+import { Store } from '../../../src/structs/Interfaces/Store';
+import { testContainer } from '../../../src/di/test.config';
+import { TYPES } from '../../../src/di/TYPES';
+import { Wagon } from '../../../src/structs/Interfaces/Wagon';
+import { getPredefinedWagonConfig } from '../../../src/structs/Actuals/Wagon/ActualWagonConfigs';
+import { WhichEnd } from '../../../src/structs/Interfaces/WhichEnd';
+chai.use(chaiAlmost());
+
+const store: Store = testContainer
+  .get<() => Store>(TYPES.FactoryOfStore)()
+  .init();
+store.clear();
+const WagonFactory: () => Wagon = () => store.create<Wagon>(TYPES.Wagon);
+
+export const A = WhichEnd.A;
+export const B = WhichEnd.B;
+
+export enum W {
+  Loco = 'locomotive',
+  Pass = 'passenger',
+  Cont = 'controlCar',
+  Comb = 'combined'
+}
+
+export function buildTrain(...wagonStrings: string[]): Wagon[] {
+  const returnedWagons: Wagon[] = [];
+  for (let wagonString of wagonStrings) {
+    returnedWagons.push(
+      WagonFactory().init(getPredefinedWagonConfig(wagonString))
+    );
+  }
+  for (let i = 1; i < returnedWagons.length; i++) {
+    returnedWagons[i - 1].getB().connect(returnedWagons[i].getA());
+  }
+
+  return returnedWagons;
+}
+
+export function expectTrainSides(
+  wagons: Wagon[],
+  sides: WhichEnd[],
+  selected?: Wagon
+) {
+  for (let i = 0; i < wagons.length; i++) {
+    expect(wagons[i].getSelectedSide()).equals(
+      sides[i],
+      'offset ' + i.toString()
+    );
+  }
+  expectTrainSelected(selected);
+}
+
+export function expectTrainSide(
+  wagon: Wagon,
+  side: WhichEnd,
+  selected?: Wagon
+) {
+  expect(wagon.getSelectedSide()).equals(side);
+  expectTrainSelected(selected);
+}
+
+export function expectTrainSelected(selected?: Wagon) {
+  if (selected !== undefined) {
+    if (selected) {
+      expect(store.getSelected().getId()).equals(selected.getId());
+    } else {
+      expect(store.getSelected()).equals(null);
+    }
+  }
+}
