68b48885278f518a0b106fd892abc6ef88543c81
turizsolt <turi.zsolt.m@gmail.com>
2021-01-16 16:50:50 +0100

    chore: adding a test level

--- src/structs/Actuals/ActualLand.ts
+++ src/structs/Actuals/ActualLand.ts
@@ -5,6 +5,7 @@ import { PassengerGenerator } from './PassengerGenerator';
 import { Store } from '../Interfaces/Store';
 import { FIRST_LEVEL } from '../../levels/FirstLevel';
 import { SECOND_LEVEL } from '../../levels/SecondLevel';
+import { TEST_LEVEL } from '../../levels/TestLevel';
 
 @injectable()
 export class ActualLand implements Land {
@@ -17,7 +18,8 @@ export class ActualLand implements Land {
     const levelId = window.location.search.slice(1);
     const levels = {
       first: FIRST_LEVEL,
-      second: SECOND_LEVEL
+      second: SECOND_LEVEL,
+      test: TEST_LEVEL
     };
 
     if (levels[levelId]) {
