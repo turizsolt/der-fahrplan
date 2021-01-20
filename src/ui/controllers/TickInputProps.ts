5971cc048de2a6d8f3fb16f05882b08273e81171
turizsolt <turi.zsolt.m@gmail.com>
2021-01-20 19:58:59 +0100

    feat: edge scrolling is now softer (linear)

---
+++ src/ui/controllers/TickInputProps.ts
@@ -0,0 +1,11 @@
+export interface TickInputProps {
+  pointerX: number;
+  pointerY: number;
+  canvasWidth: number;
+  canvasHeight: number;
+  targetX: number;
+  targetZ: number;
+  fromX: number;
+  fromY: number;
+  fromZ: number;
+}
