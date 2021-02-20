1d99183dd01137fddcb4910d22ac6f44579c816a
turizsolt <turi.zsolt.m@gmail.com>
2021-02-21 00:37:19 +0100

    change: test case with statement (switching a switch)

--- src/levels/TestFw.ts
+++ src/levels/TestFw.ts
@@ -1,49 +1,34 @@
 export const TestFw = {
   data: [
     {
-      id: 'jA58yT_Hz',
+      id: 'N4mC_nTH9',
       type: 'TrackJoint',
-      ray: {
-        x: -10,
-        y: 0,
-        z: 20,
-        dirXZ: 0
-      },
+      ray: { x: -10, y: 0, z: 10, dirXZ: 0 },
       A: null,
-      B: {
-        track: 'Hpis46DQlw',
-        whichEnd: 'B'
-      }
+      B: { track: 'yKIEnpoofM', whichEnd: 'A' }
     },
     {
-      id: '8k3FdjfOtE',
+      id: 'KhlZbRGyKy',
       type: 'TrackJoint',
-      ray: {
-        x: -10,
-        y: 0,
-        z: 40,
-        dirXZ: 0
-      },
-      A: {
-        track: 'Hpis46DQlw',
-        whichEnd: 'A'
-      },
+      ray: { x: -10, y: 0, z: 30, dirXZ: 0 },
+      A: { track: 'yKIEnpoofM', whichEnd: 'F' },
       B: null
     },
     {
-      id: 'Hpis46DQlw',
-      type: 'Track',
-      segment: [
-        {
-          x: -10,
-          y: 0,
-          z: 40
-        },
-        {
-          x: -10,
-          y: 0,
-          z: 20
-        }
+      id: 'nBrBAeJR_',
+      type: 'TrackJoint',
+      ray: { x: 0, y: 0, z: 30, dirXZ: 0.7853981633974483 },
+      A: { track: 'yKIEnpoofM', whichEnd: 'E' },
+      B: null
+    },
+    {
+      id: 'yKIEnpoofM',
+      type: 'TrackSwitch',
+      segmentE: [{ x: -10, y: 0, z: 10 }, { x: -10, y: 0, z: 30 }],
+      segmentF: [
+        { x: -10, y: 0, z: 10 },
+        { x: -10, y: 0, z: 20 },
+        { x: 0, y: 0, z: 30 }
       ]
     }
   ],
@@ -51,37 +36,27 @@ export const TestFw = {
     alpha: 0,
     beta: 0.8,
     radius: 70,
-    target: {
-      x: 0,
-      y: 0,
-      z: 30
-    },
-    position: {
-      x: 50.214926362966594,
-      y: 48.769469654301574,
-      z: 30
-    }
+    target: { x: 0, y: 0, z: 30 },
+    position: { x: 50.214926362966594, y: 48.769469654301574, z: 30 }
   },
   _version: 1,
   _format: 'fahrplan',
   actions: [
     {
-      type: 'assertion',
-      object: 'Hpis46DQlw',
-      function: 'isEmpty',
-      params: [],
-      equalsTo: true
+      type: 'statement',
+      object: 'yKIEnpoofM',
+      function: 'switch',
+      params: []
+    },
+    {
+      type: 'statement',
+      object: 'yKIEnpoofM',
+      function: 'switch',
+      params: []
     },
     {
       type: 'assertion',
-      object: 'Hpis46DQlw',
-      function: 'isEmpty',
-      params: [],
-      equalsTo: true
-    },
-    {
-      type: 'assertion',
-      object: 'Hpis46DQlw',
+      object: 'yKIEnpoofM',
       function: 'isEmpty',
       params: [],
       equalsTo: true
