fd59eb6618ce295889eba1c66959e6067057c0d8
turizsolt <turi.zsolt.m@gmail.com>
2021-02-22 16:30:03 +0100

    feat: can run multiple visual test cases

---
+++ test/visual/visual.ts
@@ -0,0 +1,18 @@
+import { testContainer } from "../../src/di/test.config";
+import { TYPES } from "../../src/di/TYPES";
+import { Store } from "../../src/structs/Interfaces/Store";
+import * as fs from 'fs';
+import * as path from 'path';
+import { expect } from "chai";
+
+const store = testContainer.get<() => Store>(TYPES.FactoryOfStore)();
+
+export function runVisualTest(testName: string): void {
+    store.clear();
+    const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, `cases/${testName}.json`), 'utf8'));
+    store.loadAll(json.data);
+    const actionStore = store.getActionStore();
+    actionStore.setActions(json.actions);
+    const verdict = actionStore.runAll();
+    expect(verdict).equals('succeded');
+}
--- test/visual/visualDemo.test.ts
+++ test/visual/visualDemo.test.ts
@@ -1,20 +1,7 @@
-import { testContainer } from "../../src/di/test.config";
-import { TYPES } from "../../src/di/TYPES";
-import { Store } from "../../src/structs/Interfaces/Store";
-import * as fs from 'fs';
-import * as path from 'path';
-import { expect } from "chai";
-
-const store = testContainer.get<() => Store>(TYPES.FactoryOfStore)();
+import { runVisualTest } from "./visual";
 
 describe('Visual test', () => {
-    it('test01', () => {
-        store.clear();
-        const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'cases/test01.json'), 'utf8'));
-        store.loadAll(json.data);
-        const actionStore = store.getActionStore();
-        actionStore.setActions(json.actions);
-        const verdict = actionStore.runAll();
-        expect(verdict).equals('succeded');
+    ['test01', 'test02'].map(test => {
+        it(test, () => { runVisualTest(test); });
     });
 });
