1032a60f4aeb0b1fe2640101426794856dc27681
turizsolt <turi.zsolt.m@gmail.com>
2021-07-19 11:11:10 +0200

    fix: undefined can be the middle trackSegment

--- src/modules/Track/DetectTrackType.ts
+++ src/modules/Track/DetectTrackType.ts
@@ -1,20 +1,22 @@
 import { almost } from "../../structs/Geometry/Almost";
+import { Coordinate } from "../../structs/Geometry/Coordinate";
 import { crossProduct } from "../../structs/Geometry/LineSegment";
 import { TrackSegmentData } from "./TrackSegmentData";
 
 export function detectTrackType(segmentData: TrackSegmentData): string {
-    if (segmentData.coordinates.length === 2) {
-        const leg = segmentData.coordinates[0].distance2d(segmentData.coordinates[1]);
+    const coordinates: Coordinate[] = segmentData.coordinates.filter(x => x);
+    if (coordinates.length === 2) {
+        const leg = coordinates[0].distance2d(coordinates[1]);
         const n = Math.round(leg / 10);
         return 'E' + n;
     } else {
-        const firstLeg = segmentData.coordinates[0].distance2d(segmentData.coordinates[1]);
-        const secondLeg = segmentData.coordinates[1].distance2d(segmentData.coordinates[2]);
+        const firstLeg = coordinates[0].distance2d(coordinates[1]);
+        const secondLeg = coordinates[1].distance2d(coordinates[2]);
         if (almost(firstLeg, secondLeg)) {
-            const diagonal = segmentData.coordinates[0].distance2d(segmentData.coordinates[2]);
+            const diagonal = coordinates[0].distance2d(coordinates[2]);
             if (almost(firstLeg * Math.sqrt(3), diagonal)) {
                 const n = Math.round(firstLeg / 10);
-                if (crossProduct(segmentData.coordinates[0], segmentData.coordinates[1], segmentData.coordinates[2]) === 1) {
+                if (crossProduct(coordinates[0], coordinates[1], coordinates[2]) === 1) {
                     return 'L' + n;
                 } else {
                     return 'R' + n;
