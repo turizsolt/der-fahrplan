5d6b57585da70f2a1220cffc0210fb5957ed53d0
turizsolt <turi.zsolt.m@gmail.com>
2021-08-01 23:50:06 +0200

    feat: diagram tab

---
+++ src/ui/controllers/DiagramPlanner.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="big-screen-one">
+        <div class="column-routes" id="route-list">
+            <div class="column-title">Route list</div>
+            <route-title :key="route.id" v-for="route in routes" :route="route"
+                :selected="selectedRoute && selectedRoute.id === route.id" candelete @click="selectRoute(route)"
+                @delete="removeRoute(route)"></route-title>
+            <div class="route-create-holder">
+                <span class="route-create" v-on:click="createRoute()">Create route</span>
+            </div>
+        </div>
+        <div class="column-route-stops" id="route-details">
+            <div class="column-title">Edit route</div>
+            <div v-if="selectedRoute">
+                <div class="route-details">
+                    Name:
+                    <input class="route-input-name" type="text" v-model="selectedRoute.name"
+                        @keyup.stop="nameChange" /><br />
+                </div>
+                <div class="route-details">
+                    Color:
+                    <input class="route-input-name" type="text" v-model="selectedRoute.color"
+                        @keyup.stop="colorChange" /><br />
+                </div>
+                <div class="route-details-stops">
+                    Stops:<br />
+                    <route-stop v-for="(stop, index) in selectedRoute.stops" :key="stop.id" :route="selectedRoute"
+                        :stop="stop" :index="index" candelete :canmove="index !== 0" @delete="deleteStop(stop)"
+                         @reverse="reverseStop(stop)" @shouldStop="shouldStop(stop)">
+                    </route-stop>
+                </div>
+                <div class="route-details route-create-holder">
+                    <span class="route-create" v-on:click="createReverseRoute(selectedRoute)">Create reverse
+                        route</span>
+                </div>
+            </div>
+        </div>
+        <route-diagram :route="selectedRoute"></route-diagram>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import { Route } from "../../structs/Scheduling/Route";
+import { Station } from "../../structs/Scheduling/Station";
+import { RouteStop } from "../../structs/Scheduling/RouteStop";
+import { TYPES } from "../../di/TYPES";
+import { getStore, getAllOfStorable, getStorable, createStorable } from "../../structs/Actuals/Store/StoreForVue";
+import { RailMap } from "../../modules/RailMap/RailMap";
+import { RailMapNode } from "../../modules/RailMap/RailMapNode";
+import { RailMapCreator } from "../../modules/RailMap/RailMapCreator";
+
+@Component
+export default class DiagramPlanner extends Vue {
+    private map: RailMap;
+    routes: any[] = [];
+    selectedRoute: any = null;
+    stations: any[] = [];
+
+    created() {
+      this.map = RailMapCreator.create(getStore());
+      this.load();
+    }
+
+    load() {
+          this.routes = getAllOfStorable<Route>(TYPES.Route)
+            .map(x => x.persistDeep());
+          this.stations = getAllOfStorable<Station>(TYPES.Station)
+            .map(x => Object.freeze(x.persistDeep()));
+            this.stations.sort((a, b) => {
+              if(a.name < b.name) return -1;
+              if(a.name > b.name) return 1;
+              return 0;
+            });
+          if (this.selectedRoute) {
+            this.selectedRoute = (getStorable(
+              this.selectedRoute.id
+            ) as Route).persistDeep()
+          }
+        };
+
+        removeRoute(vRoute) {
+          const route = getStorable(vRoute.id) as Route;
+          route.remove();
+          this.selectedRoute = null;
+          this.load();
+        };
+
+        createRoute() {
+          const route = createStorable<Route>(TYPES.Route);
+          route.init();
+          this.load();
+          const routeData = this.routes.find(x => x.id === route.getId());
+          this.selectRoute(routeData);
+        };
+
+        createReverseRoute(vRouteFrom) {
+          const routeFrom = getStorable(vRouteFrom.id) as Route;
+          const route = createStorable<Route>(TYPES.Route);
+          route.init();
+          route.setName(routeFrom.getName());
+          for (let stopFrom of [...routeFrom.getWaypoints()].reverse()) {
+            const stop = createStorable<RouteStop>(TYPES.RouteStop);
+            stop.init(stopFrom.getStation(), stopFrom.getPlatform());
+            route.addWaypoint(stop);
+          }
+          this.load();
+          const routeData = this.routes.find(x => x.id === route.getId());
+          this.selectRoute(routeData);
+        };
+
+        selectRoute(route) {
+          this.selectedRoute = route;
+          this.load();
+        };
+
+        deleteStop(vStop) {
+          const stop = getStorable(vStop.id) as RouteStop;
+          const route = getStorable(this.selectedRoute.id) as Route;
+          route.removeStop(stop);
+
+          this.checkFirstAndLastStop();
+          this.load();
+        };
+
+        reverseStop(vStop) {
+          const stop = getStorable(vStop.id) as RouteStop;
+          stop.toggleReverseStop();
+          this.load();
+        };
+
+        shouldStop(vStop) {
+          const stop = getStorable(vStop.id) as RouteStop;
+          stop.setShouldStop(!stop.getShouldStop());
+
+          this.checkFirstAndLastStop();
+          this.load();
+        }
+
+        checkFirstAndLastStop() {
+          const route = getStorable(this.selectedRoute.id) as Route;
+          const stops = route.getWaypoints();
+          if(stops.length > 0) {
+            const firstStop = stops[0];
+            if(!firstStop.getShouldStop()) {
+              firstStop.setShouldStop(true);
+            }
+
+            if(stops.length > 1) {
+              const lastStop = stops[stops.length-1];
+              if(!lastStop.getShouldStop()) {
+                lastStop.setShouldStop(true);
+              }
+            }
+          }
+        }
+
+        addStop(evt, stationId) {
+          if (this.selectedRoute) {
+            const route = getStorable(this.selectedRoute.id) as Route;
+            const station = getStorable(stationId) as Station;
+
+            let addingStations:RailMapNode[] = [station];
+            if(route.getLastWaypoint()) {
+              const result = this.map.getShortestPath(route.getLastWaypoint(), station);
+              addingStations = result.slice(1);
+            }
+            const last = addingStations[addingStations.length-1];
+
+            for(let station of addingStations) {
+              const stop = createStorable<RouteStop>(TYPES.RouteStop);
+              stop.init(
+                station
+              );
+              if(station !== last) {
+                stop.setShouldStop(false);
+              }
+              route.addWaypoint(stop);
+            }
+            this.load();
+          }
+        };
+
+        nameChange(event) {
+          const route = getStorable(this.selectedRoute.id) as Route;
+          route.setName(event.target.value);
+          this.load();
+        };
+
+        colorChange(event) {
+          const route = getStorable(this.selectedRoute.id) as Route;
+          route.setColor(event.target.value);
+          this.load();
+        };
+}
+</script>
+
+<style>
+.big-screen-one {
+    display: flex;
+    height: 100%;
+}
+
+.column-routes {
+  width: 200px;
+  min-width: 200px;
+}
+
+.column-route-stops {
+  width: 280px;
+  min-width: 280px;
+}
+
+.column-add-stops {
+  overflow-y: scroll;
+}
+
+
+</style>
---
+++ src/ui/controllers/RouteDiagram.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="map-field">
+    Ide jon minden, Diagram
+    <hr />
+    {{route}}
+    <!--
+    <div v-for="edge in edges" class="edge" :style="edge.style"></div>  
+    <div v-for="node in nodes" class="node-text" :style="{left: node.textLeft, top: node.textTop, display: node.textDisplay}">{{node.getName()}}</div>  
+    <div v-for="node in nodes" @click="$emit('addStop', $event, node.getId())" class="node" :style="{left: node.left, top: node.top, width: node.size, height: node.size, backgroundColor: node.bgColor}" :title="node.getName()"></div>  
+    -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { TYPES } from "../../di/TYPES";
+import { Route } from "../../structs/Scheduling/Route";
+import { Station } from "../../structs/Scheduling/Station";
+import { getStore } from "../../structs/Actuals/Store/StoreForVue";
+import { RailMap } from "../../modules/RailMap/RailMap";
+import { RailMapCreator } from "../../modules/RailMap/RailMapCreator";
+
+@Component
+export default class RouteDiagram extends Vue {
+  private map: RailMap;
+  @Prop() route: any;
+  nodes: any[] = [];
+  edges: any[] = [];
+
+  constructor() {
+    super();
+    
+    this.init = this.init.bind(this);
+
+    setTimeout(() => {
+      this.init();
+    }, 0);
+  }
+
+  init() {
+      this.map = RailMapCreator.create(getStore());
+
+      this.nodes = this.map.getNodes();
+      const gotEdges = this.map.getEdges();
+      this.edges = [];
+      for(let key of Object.keys(gotEdges)) {
+        this.edges.push(gotEdges[key]);
+      }
+
+      const mapField = document.getElementById('map-field');
+      const w = mapField.clientWidth - 40;
+      const h = mapField.clientHeight - 40;
+      const bounds = this.map.getBounds();
+      const wRange = bounds.maxX - bounds.minX;
+      const hRange = bounds.maxZ - bounds.minZ;
+      const m = Math.max(wRange / w, hRange / h);
+
+      // center at 10,10 !!!
+      const left = (c) => (c.x - bounds.minX) / m;
+      const top = (c) => (c.z - bounds.minZ) / m;
+      const dist = (c) => c / m;
+
+      for(let i=0;i < this.nodes.length; i++) {
+        const coord = this.nodes[i].getCoord();
+        const size = this.nodes[i].getType() === TYPES.Station ? 20 : 10;
+        this.nodes[i].left = (20+left(coord)-size/2)+'px';
+        this.nodes[i].top = (20+top(coord)-size/2)+'px';
+        this.nodes[i].size = (size)+'px';
+        this.nodes[i].textLeft = (20+left(coord)+size/2)+'px';
+        this.nodes[i].textTop = (20+top(coord)+size/2)+'px';
+        this.nodes[i].textDisplay = this.nodes[i].getType() === TYPES.Station ? 'block' : 'none';
+        this.nodes[i].bgColor = this.nodes[i].getType() === TYPES.Station ? 'aqua' : 'gray';
+      }
+
+      for(let i=0;i < this.edges.length; i++) {
+        const coord = this.edges[i].from.getCoord().midpoint(this.edges[i].to.getCoord());
+        const wi = dist(this.edges[i].from.getCoord().distance2d(this.edges[i].to.getCoord()));
+        const r = this.edges[i].from.getCoord().whichDir2d(this.edges[i].to.getCoord());
+        const e = Math.min(this.edges[i].count, 4);
+        this.edges[i].style = {
+          left: (20+left(coord)-(e*3/2))+'px',
+          top: (20+top(coord)-wi/2)+'px',
+          height: wi+'px',
+          width: (e*3)+'px',
+          transform: 'rotate('+(-r)+'rad)'
+        };
+      };
+  }
+
+  update() {}
+}
+</script>
+
+<style scoped>
+#map-field {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  border: 1px solid red;
+}
+
+/*
+.node {
+  border-radius: 50%;
+  border: 1px solid blue;
+  position: absolute;
+}
+
+.node-text {
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.75);
+}
+
+.edge {
+  background-color: black;
+  position: absolute;
+}
+
+.node:hover {
+  cursor: pointer;
+}
+*/
+</style>
