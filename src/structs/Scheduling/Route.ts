986014ac12050f630a363baaab2258d6fa293e42
turizsolt <turi.zsolt.m@gmail.com>
2022-01-23 01:07:34 +0100

    refact: annotated Route what to change

--- src/structs/Scheduling/Route.ts
+++ src/structs/Scheduling/Route.ts
@@ -21,19 +21,40 @@ export interface Route extends BaseStorable {
 
     // from the old version
 
+    // hard part, a lot of rewrite will be needed - in 55 places to be exact
+    // todo rewrite
     getStops(): RouteStop[];
     getWaypoints(): RouteStop[];
     addWaypoint(stop: RouteStop): void;
     removeStop(stop: RouteStop): void;
 
+    // trip connector - to filter routes by its first and last station
+    // todo RouteVariant
     getFirstStation(): Station;
     getLastStation(): Station;
+
+    // nope
+    // todo delete
     getFirstWaypoint(): RailMapNode;
+
+    // diagram planner - add stop to plan the route from the last to the added
+    // route planner - the same code - one of them is copy pasta
+    // todo RouteVariant
     getLastWaypoint(): RailMapNode;
+
+    // route planner - add stop
+    // todo RouteVariant
     getLastStop(): RouteStop;
 
+    // to determine should be on the graph or not
+    // todo - rewrite
     hasCommonEdgeWith(route: Route): boolean;
+
+    // helper to the former
+    // todo - rewrite
     getEdges(): { from: RailMapNode; to: RailMapNode }[];
 
+    // to write out, in persist it is important maybe
+    // todo - rewrite
     getDetailedName(): string;
 }
